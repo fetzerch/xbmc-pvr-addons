8d19718b16ad117239d224834b14319f9006fe4a
janbar <jlbarriere68@gmail.com>
2013-07-27 18:50:11 +0200

    [mythtv-cmyth] Add an abstract class MythFile::StreamHandler
    BasicStreamHandler is the default implementation for StreamHandler. It provides basic Read, Seek, Position and Length from old class MythFile. MythFile::StreamHandler or BasicStreamHandler can be extended to provide most advanced services. The method  MythFile::RegisterStreamHandler() allows to plug a new stream handler.

--- addons/pvr.mythtv.cmyth/src/cppmyth/MythFile.cpp
+++ addons/pvr.mythtv.cmyth/src/cppmyth/MythFile.cpp
@@ -24,14 +24,17 @@
 MythFile::MythFile()
   : m_file_t(new MythPointer<cmyth_file_t>())
   , m_conn(MythConnection())
+  , m_stream()
 {
 }
 
-MythFile::MythFile(cmyth_file_t myth_file, MythConnection conn)
+MythFile::MythFile(cmyth_file_t myth_file, MythConnection &conn)
   : m_file_t(new MythPointer<cmyth_file_t>())
   , m_conn(conn)
+  , m_stream(new BasicStreamHandler())
 {
   *m_file_t = myth_file;
+  m_stream->Register(*this);
 }
 
 bool MythFile::IsNull() const
@@ -43,8 +46,7 @@ bool MythFile::IsNull() const
 
 unsigned long long MythFile::Length()
 {
-  int64_t length = cmyth_file_length(*m_file_t);
-  return length > 0 ? length : 0;
+  return m_stream->Length();
 }
 
 void MythFile::UpdateLength(unsigned long long length)
@@ -56,21 +58,71 @@ void MythFile::UpdateLength(unsigned long long length)
 
 int MythFile::Read(void *buffer, unsigned int length)
 {
-  int bytesRead;
-  bytesRead = cmyth_file_read(*m_file_t, static_cast< char * >(buffer), length);
-  return bytesRead;
+  return m_stream->Read(buffer, length);
 }
 
 long long MythFile::Seek(long long offset, int whence)
 {
-  long long retval = 0;
-  retval = cmyth_file_seek(*m_file_t, offset, whence);
-  return retval;
+  return m_stream->Seek(offset, whence);
 }
 
 unsigned long long MythFile::Position()
 {
+  return m_stream->Position();
+}
+
+void MythFile::RegisterStreamHandler(StreamHandler* stream)
+{
+  stream->Register(*this);
+  m_stream.reset(stream);
+}
+
+MythFile::StreamHandler *MythFile::GetStreamHandler()
+{
+  return m_stream.get();
+}
+
+///////////////////////////////////////////////////////////////////////////////
+////
+//// Abstract stream handler
+////
+///////////////////////////////////////////////////////////////////////////////
+void MythFile::StreamHandler::Register(const MythFile &file)
+{
+  m_myth_file = *(file.m_file_t);
+}
+
+inline MythFile::StreamHandler::~StreamHandler() { }
+
+///////////////////////////////////////////////////////////////////////////////
+////
+//// Basic stream handler
+////
+///////////////////////////////////////////////////////////////////////////////
+unsigned long long BasicStreamHandler::Length()
+{
+  long long retval;
+  retval = cmyth_file_length(m_myth_file);
+  return retval > 0 ? retval : 0;
+}
+
+int BasicStreamHandler::Read(void *buffer, unsigned int length)
+{
+  int bytesRead;
+  bytesRead = cmyth_file_read(m_myth_file, static_cast< char * >(buffer), length);
+  return bytesRead;
+}
+
+long long BasicStreamHandler::Seek(long long offset, int whence)
+{
+  long long retval;
+  retval = cmyth_file_seek(m_myth_file, offset, whence);
+  return retval;
+}
+
+unsigned long long BasicStreamHandler::Position()
+{
   unsigned long long retval = 0;
-  retval = cmyth_file_position(*m_file_t);
+  retval = cmyth_file_position(m_myth_file);
   return retval;
 }
